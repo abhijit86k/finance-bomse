5b1820974d72f196b8cc6d80ddf71ad32b9171e5
Abhijit (Toshiba) <abhijit86k@gmail.com>
2017-08-03 23:40:26 -0500

    Update nsetools
    - Timestamp in IST
    - Fixed failure output

--- nsetoolsfetch.py
+++ nsetoolsfetch.py
@@ -2,6 +2,8 @@
 
 #imports
 import sys, logging, json
+from datetime import datetime
+from datetime import timedelta
 
 #logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 #logging.basicConfig(stream=sys.stderr, level=logging.CRITICAL)
@@ -28,14 +30,22 @@ if nse.is_valid_code(scrip):
 	logging.info('Scrip name is valid')
 	#Add Success attrib in JSON
 	q=nse.get_quote(scrip, as_json=True)
+	#q = '{ }';
 	quotedata = json.loads(q)
 	quotedata['Success'] = 1;
+	#Add timestamp:
+	#quote_time =  datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S+0000");
+
+	T_UTC = datetime.utcnow()
+	T_in  = T_UTC + timedelta(hours=5, minutes=30)
+	quote_time =  T_in.strftime("%s");
+	quotedata['timestamp'] = quote_time;
 	q=json.dumps(quotedata);
 	print(q)
 else:
 	logging.info('Invalid scrip name. Check https://www.nseindia.com to get correct scrip name.')
 	#Create JSON with fail attrib
-	FailObject = {'Success': 0};
-	FailObject_JSON = json.dumps(FailObject);
-	print(FailObject);
+	FailObjectString = '{"Success": 0}';
+	#FailObject_JSON = json.dumps(FailObject);
+	print(FailObjectString);
 exit()
