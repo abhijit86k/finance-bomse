f6c7cc6e0293a45f3cbde8e057c88d07fba2265e
Abhijit (Toshiba) <abhijit86k@gmail.com>
2017-08-03 22:46:33 -0500

    Update nsetoolsfetch script
    Add success value in valid JSON

--- nsetoolsfetch.py
+++ nsetoolsfetch.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
 #imports
-import sys, logging
+import sys, logging, json
 
 #logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 #logging.basicConfig(stream=sys.stderr, level=logging.CRITICAL)
-logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+#logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 argnum = len(sys.argv)
 scrip = str(sys.argv[1])
 
-logging.debug('A debug message!')
-logging.info('A debug message!')
-logging.critical('This is a critical message')
+#logging.debug('A debug message!')
+#logging.info('A debug message!')
+#logging.critical('This is a critical message')
 
 if argnum != 2:
 	logging.info('More than one argument to script. Currently not supported')	
@@ -26,10 +26,16 @@ nse = Nse()
 
 if nse.is_valid_code(scrip):
 	logging.info('Scrip name is valid')
+	#Add Success attrib in JSON
+	q=nse.get_quote(scrip, as_json=True)
+	quotedata = json.loads(q)
+	quotedata['Success'] = 1;
+	q=json.dumps(quotedata);
+	print(q)
 else:
 	logging.info('Invalid scrip name. Check https://www.nseindia.com to get correct scrip name.')
-	exit()
-
-q=nse.get_quote(scrip, as_json=True)
-
-print(q)
+	#Create JSON with fail attrib
+	FailObject = {'Success': 0};
+	FailObject_JSON = json.dumps(FailObject);
+	print(FailObject);
+exit()
